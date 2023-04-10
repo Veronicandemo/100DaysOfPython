2fc7e0c5dd5b0b270d0d31e20f5adae8ca73ad65
Veronicandemo <ndemoveronica@gmail.com>
2023-04-10 09:34:09 +0300

    control flow

--- Day-3-control-flow/control.py
+++ Day-3-control-flow/control.py
@@ -1,108 +1,108 @@
 
 # Free for mid life crisis that is between 45 and 55
-# print('Welcme to the Rollercoaster')
-# height = int(input('What is your height in cm ? '))
-# bill = 0
+print('Welcme to the Rollercoaster')
+height = int(input('What is your height in cm ? '))
+bill = 0
 
-# if height >= 120:
-#     print('You can the rollercoaster')
-#     age = int(input('What is your age ? '))
-#     if age < 12:
-#         bill = 5
-#         print('Child ticket pay $ 5.')
-#     elif age <= 18:
-#         bill = 7
-#         print('Youth ticket pay $ 7.')
-#     elif age >= 45 and age <= 55:
-#         print('Everything is gonna be okay, have a free ride on us')
-#     else:
-#         bill = 12
-#         print('Adult ticket pay $12 ')
+if height >= 120:
+    print('You can the rollercoaster')
+    age = int(input('What is your age ? '))
+    if age < 12:
+        bill = 5
+        print('Child ticket pay $ 5.')
+    elif age <= 18:
+        bill = 7
+        print('Youth ticket pay $ 7.')
+    elif age >= 45 and age <= 55:
+        print('Everything is gonna be okay, have a free ride on us')
+    else:
+        bill = 12
+        print('Adult ticket pay $12 ')
 
-#     wants_photo = input('Do you want a photo taken? Type Y or N.')
-#     if wants_photo == 'Y':
-#        bill += 3
-#     print(f'Your final bill is {bill}')
-# else:
-#     print('Sorry, you have to grow taller before you can ride.')
+    wants_photo = input('Do you want a photo taken? Type Y or N.')
+    if wants_photo == 'Y':
+       bill += 3
+    print(f'Your final bill is {bill}')
+else:
+    print('Sorry, you have to grow taller before you can ride.')
 
 
 # # A program that checks whether the number entered is /odd or Even
-# number = int(input('Which number do you want to check ? '))
+number = int(input('Which number do you want to check ? '))
 
-# if number % 2 == 0:
-#     print(f'{number} is even')
-# else:
-#     print(f'{number} is odd')
+if number % 2 == 0:
+    print(f'{number} is even')
+else:
+    print(f'{number} is odd')
 
 
-# height = float(input('Enter your height in meters: '))
-# weight = float(input('Enter your weight in kgs: '))
+height = float(input('Enter your height in meters: '))
+weight = float(input('Enter your weight in kgs: '))
 
-# bmi = round(weight / (height**2), 1)
+bmi = round(weight / (height**2), 1)
 
-# if bmi < 18.5:
-#     print(f'Your bmi is : {bmi}, you are underweight.')
-# elif bmi < 25:
-#     print(f'Your bmi is {bmi}, you have normal weight.')
-# elif bmi < 30:
-#     print(f'Your bmi is {bmi}, you are overweight.')
-# elif bmi < 35:
-#     print(f'Your bmi is {bmi}, you are obese.')
-# else:
-#     print(f'Your bmi is {bmi}, your are clinically obese')
+if bmi < 18.5:
+    print(f'Your bmi is : {bmi}, you are underweight.')
+elif bmi < 25:
+    print(f'Your bmi is {bmi}, you have normal weight.')
+elif bmi < 30:
+    print(f'Your bmi is {bmi}, you are overweight.')
+elif bmi < 35:
+    print(f'Your bmi is {bmi}, you are obese.')
+else:
+    print(f'Your bmi is {bmi}, your are clinically obese')
 
 
 # # Write a program that works out whether if a given year is a leap year.
 # #  A normal year has 365 days, leap years have 366, with an extra day in February.
 
-# year = int(input('Which year do you want to check ? '))
+year = int(input('Which year do you want to check ? '))
 
-# if year % 4 == 0:
-#     if year % 100 == 0:
-#       if year % 400 == 0:
-#        print(f'{year} is a leap year.')
-#       else:
-#         print(f'{year}  is not a leap year')
-#     else:
-#      print(f'{year} is a leap year.')
-# else:
-#     print(f'{year}  is not a leap year')
+if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+       print(f'{year} is a leap year.')
+      else:
+        print(f'{year}  is not a leap year')
+    else:
+     print(f'{year} is a leap year.')
+else:
+    print(f'{year}  is not a leap year')
 
 
-# print("Welcome to Python Pizza Deliveries!")
-# size = input("What size pizza do you want? S, M, or L ")
-# add_pepperoni = input("Do you want pepperoni? Y or N ")
-# extra_cheese = input("Do you want extra cheese? Y or N ")
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
 
-# # Based on a user's order, work out their final bill.
+# Based on a user's order, work out their final bill.
 
-# # Small Pizza: $15
+# Small Pizza: $15
 
-# # Medium Pizza: $20
+# Medium Pizza: $20
 
-# # Large Pizza: $25
+# Large Pizza: $25
 
-# # Pepperoni for Small Pizza: +$2
+# Pepperoni for Small Pizza: +$2
 
-# # Pepperoni for Medium or Large Pizza: +$3
+# Pepperoni for Medium or Large Pizza: +$3
 
-# # Extra cheese for any size pizza: + $1
-# bill = 0
-# if size == "S":
-#         bill += 15
-# elif size == "M": 
-#         bill += 20
-# else:
-#         bill += 25
-# if add_pepperoni == "Y":
-#     if size == "S":
-#         bill += 2  
-# else:
-#     bill += 3 
-# if extra_cheese  == "Y":
-#     bill += 1
-# print(f"Your final bill is: ${bill}.")
+# Extra cheese for any size pizza: + $1
+bill = 0
+if size == "S":
+        bill += 15
+elif size == "M": 
+        bill += 20
+else:
+        bill += 25
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2  
+else:
+    bill += 3 
+if extra_cheese  == "Y":
+    bill += 1
+print(f"Your final bill is: ${bill}.")
 
 # # LOVE CALCULATOR
 '''
